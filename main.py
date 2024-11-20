605effeabf8f19cefbdbe4155f49b040aba34362
dinesh-0602 <rainadinesh94@gmail.com>
2024-11-20 09:58:11 +0530

    Readme changes

---
+++ main.py
@@ -0,0 +1,248 @@
+! wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-CV0101EN-Coursera/images%20/images_part_5/DLguys.jpeg
+! wget https://www.ajot.com/images/uploads/article/quantas-car-v-plane-TAKE-OFF.jpg
+! wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-CV0101EN-Coursera/images%20/images_part_5/istockphoto-187786732-612x612.jpeg
+! wget https://cdn.webrazzi.com/uploads/2015/03/andrew-ng1.jpg
+! pip install -q condacolab
+import condacolab
+condacolab.install()
+
+! conda install pytorch=1.1.0 torchvision -c pytorch -y
+
+import torchvision
+from torchvision import  transforms
+import torch
+from torch import no_grad
+
+import cv2
+import numpy as np
+from PIL import Image
+import matplotlib.pyplot as plt
+
+
+def get_predictions(pred, threshold=0.8, objects=None):
+    predicted_classes= [(COCO_INSTANCE_CATEGORY_NAMES[i],p,[(box[0], box[1]), (box[2], box[3])]) for i,p,box in zip(list(pred[0]['labels'].numpy()),pred[0]['scores'].detach().numpy(),list(pred[0]['boxes'].detach().numpy()))]
+    predicted_classes=[  stuff  for stuff in predicted_classes  if stuff[1]>threshold ]
+
+    if objects  and predicted_classes :
+        predicted_classes=[ (name, p, box) for name, p, box in predicted_classes if name in  objects ]
+    return predicted_classes
+
+
+def draw_box(pred_class, img, rect_th=2, text_size=0.5, text_th=2, download_image=False, img_name="img"):
+
+    image = (np.clip(cv2.cvtColor(np.clip(img.numpy().transpose((1, 2, 0)), 0, 1), cv2.COLOR_RGB2BGR), 0, 1) * 255).astype(np.uint8).copy()
+
+    for predicted_class in pred_class:
+
+      label=predicted_class[0]
+      probability=predicted_class[1]
+      box=predicted_class[2]
+      t = round(box[0][0].tolist())
+      l = round(box[0][1].tolist())
+      r = round(box[1][0].tolist())
+      b = round(box[1][1].tolist())
+
+      # Giving brief information about rectange, class and probability.
+      from colorama import Fore
+      from colorama import Style
+      print(f"\nLabel: {Fore.GREEN}{label}{Style.RESET_ALL}")
+      print(f"Box coordinates: {t}, {l}, {r}, {b}")
+      print(f"Probability: {probability}")
+
+      # Drawing rectangle and adding text on the picture based on their class and size.
+      cv2.rectangle(image, (t, l), (r, b), (0, 255, 0), rect_th)
+      cv2.rectangle(image, (t, l), (t+110, l+17), (255, 255, 255), -1)
+      cv2.putText(image, label, (t+10, l+12),  cv2.FONT_HERSHEY_SIMPLEX,
+                  text_size, (0,255,0), thickness=text_th)
+      cv2.putText(image, label+": "+str(round(probability, 2)),
+                  (t+10, l+12),  cv2.FONT_HERSHEY_SIMPLEX, text_size,
+                  (0, 255, 0),thickness=text_th)
+
+    # Plotting image
+    image = np.array(image)
+    plt.figure(figsize=(15, 10))
+    plt.imshow(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
+    if download_image:
+      plt.savefig(f'{img_name}.png')
+    else:
+      pass
+    plt.show()
+
+    del(img)
+    del(image)
+
+def save_RAM(image_=False):
+    global image, img, pred
+    torch.cuda.empty_cache()
+    del(img)
+    del(pred)
+    if image_:
+        image.close()
+        del(image)
+
+model_ = torchvision.models.detection.fasterrcnn_resnet50_fpn(pretrained=True)
+model_.eval()
+
+for name, param in model_.named_parameters():
+    param.requires_grad = False
+print("done")
+
+
+
+def model(x):
+    with torch.no_grad():
+        yhat = model_(x)
+    return yhat
+
+COCO_INSTANCE_CATEGORY_NAMES = [
+    '__background__', 'person', 'bicycle', 'car', 'motorcycle', 'airplane', 'bus',
+    'train', 'truck', 'boat', 'traffic light', 'fire hydrant', 'N/A', 'stop sign',
+    'parking meter', 'bench', 'bird', 'cat', 'dog', 'horse', 'sheep', 'cow',
+    'elephant', 'bear', 'zebra', 'giraffe', 'N/A', 'backpack', 'umbrella', 'N/A', 'N/A',
+    'handbag', 'tie', 'suitcase', 'frisbee', 'skis', 'snowboard', 'sports ball',
+    'kite', 'baseball bat', 'baseball glove', 'skateboard', 'surfboard', 'tennis racket',
+    'bottle', 'N/A', 'wine glass', 'cup', 'fork', 'knife', 'spoon', 'bowl',
+    'banana', 'apple', 'sandwich', 'orange', 'broccoli', 'carrot', 'hot dog', 'pizza',
+    'donut', 'cake', 'chair', 'couch', 'potted plant', 'bed', 'N/A', 'dining table',
+    'N/A', 'N/A', 'toilet', 'N/A', 'tv', 'laptop', 'mouse', 'remote', 'keyboard', 'cell phone',
+    'microwave', 'oven', 'toaster', 'sink', 'refrigerator', 'N/A', 'book',
+    'clock', 'vase', 'scissors', 'teddy bear', 'hair drier', 'toothbrush'
+]
+len(COCO_INSTANCE_CATEGORY_NAMES)
+
+
+img_path='andrew-ng1.jpg'
+half = 0.5
+image = Image.open(img_path)
+
+image.resize([int(half * s) for s in image.size] )
+plt.figure(figsize=(15, 10))
+plt.imshow(image)
+plt.show()
+
+
+transform = transforms.Compose([transforms.ToTensor()])
+img = transform(image)
+img
+pred = model([img])
+
+len(pred[0]['labels'])
+pred[0]['labels']
+
+pred[0]['scores']
+
+
+index=pred[0]['labels'][0].item()
+COCO_INSTANCE_CATEGORY_NAMES[index]
+
+
+
+bounding_box=pred[0]['boxes'][0].tolist()
+bounding_box
+
+
+t, l, r, b = [round(x) for x in bounding_box]
+print(t, l, r, b)
+
+
+img_plot=(np.clip(cv2.cvtColor(np.clip(img.numpy().transpose((1, 2, 0)), 0, 1), cv2.COLOR_RGB2BGR), 0, 1) * 255).astype(np.uint8)
+cv2.rectangle(img_plot, (t, l), (r, b), (0, 255, 0), 10) # Draw Rectangle with the coordinates
+plt.figure(figsize=(15, 10))
+plt.imshow(cv2.cvtColor(img_plot, cv2.COLOR_BGR2RGB))
+plt.show()
+del img_plot, t, l, r, b
+
+
+pred_class=get_predictions(pred, objects="person")
+draw_box(pred_class, img)
+
+del pred_class
+
+get_predictions(pred, threshold=1, objects="person")
+
+
+
+pred_thresh=get_predictions(pred, threshold=0.98, objects="person")
+draw_box(pred_thresh, img, download_image=True, img_name="andrew_BOX")
+del pred_thresh
+save_RAM(image_=True)
+
+img_path='DLguys.jpeg'
+image = Image.open(img_path)
+image.resize([int(half * s) for s in image.size])
+plt.figure(figsize=(15, 10))
+plt.imshow(np.array(image))
+plt.show()
+
+
+img = transform(image)
+pred = model([img])
+pred_thresh=get_predictions(pred, threshold=0.8)
+draw_box(pred_thresh, img, rect_th=1, text_size= 0.5, text_th=1)
+
+del pred_thresh
+
+
+pred_obj=get_predictions(pred,objects="person")
+draw_box(pred_obj, img, rect_th=1,text_size= 0.5, text_th=1, download_image=True, img_name="dl_guys_IBM_BOX")
+
+del pred_obj
+
+pred_thresh=get_predictions(pred,threshold=0.01)
+draw_box(pred_thresh, img, rect_th= 1, text_size=0.5, text_th=1)
+
+del pred_thresh
+
+
+save_RAM(image_=True)
+
+img_path='istockphoto-187786732-612x612.jpeg'
+image = Image.open(img_path)
+image.resize( [int(half * s) for s in image.size])
+plt.figure(figsize=(15, 10))
+plt.imshow(np.array(image))
+plt.show()
+del img_path
+
+img = transform(image)
+pred = model([img])
+pred_thresh=get_predictions(pred,threshold=0.97)
+draw_box(pred_thresh,img,rect_th=1, text_size=1, text_th=1, download_image=True, img_name="dog_cat_bird_BOX")
+
+del pred_thresh
+
+# img = transform(image)
+# pred = model([img])
+pred_obj=get_predictions(pred, objects=["dog","cat"])
+draw_box(pred_obj, img,rect_th=1, text_size= 0.5, text_th=1)
+del pred_obj
+
+
+# img = transform(image)
+# pred = model([img])
+pred_thresh=get_predictions(pred, threshold=0.70, objects=["dog", "cat"])
+draw_box(pred_thresh, img, rect_th= 1, text_size=1, text_th=1)
+
+del pred_thresh
+
+save_RAM(image_=True)
+
+
+
+img_path='quantas-car-v-plane-TAKE-OFF.jpg'
+image = Image.open(img_path)
+image.resize( [int(half * s) for s in image.size])
+plt.figure(figsize=(15, 10))
+plt.imshow(np.array(image))
+plt.show()
+del img_path
+
+img = transform(image)
+pred = model([img])
+pred_thresh=get_predictions(pred, threshold=0.997)
+draw_box(pred_thresh, img, download_image=True, img_name="car_plane_BOX")
+del pred_thresh
+
+save_RAM(image_=True)
+from pathlib import Path
+print("Directory Path:", Path().absolute())
--- readme.md
+++ readme.md
@@ -23,3 +23,80 @@ To set up the environment for this project, follow these steps:
    ```bash
    git clone https://github.com/dinesh-2410/DL-mini.git
    cd DL-mini
+   ```
+
+2. **Install required packages:**
+
+   ```bash
+   ! pip install -q condacolab
+   import condacolab
+   condacolab.install()
+   ! conda install pytorch=1.1.0 torchvision -c pytorch -y
+   ```
+3. **Install additional dependencies:**
+
+   ```bash
+   pip install opencv-python numpy pillow matplotlib
+   ```
+## Usage
+
+1. **Download images:**
+
+   Use the following commands to download sample images:
+   ```bash
+   !wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-CV0101EN-Coursera/images%20/images_part_5/DLguys.jpeg
+   !wget https://www.ajot.com/images/uploads/article/quantas-car-v-plane-TAKE-OFF.jpg
+   !wget https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-CV0101EN-Coursera/images%20/images_part_5/istockphoto-187786732-612x612.jpeg
+   !wget https://cdn.webrazzi.com/uploads/2015/03/andrew-ng1.jpg
+   ```
+
+2. **Run the object detection script:**
+
+   Execute the main script to perform object detection on the downloaded images.
+   ```bash
+   python main.py
+   ```
+
+## Functions
+   `get_predictions(pred, threshold=0.8, objects=None)`
+
+   * Filters predictions based on a confidence threshold and specified object classes.
+
+   `draw_box(pred_class, img, rect_th=2, text_size=0.5, text_th=2, download_image=False, img_name="img")`
+
+   * Draws bounding boxes around detected objects in the image.
+
+   `save_RAM(image_=False)`
+
+   * Releases memory used by images and predictions.
+
+## Model Details
+The project utilizes the Faster R-CNN model with a ResNet-50 backbone pre-trained on the COCO dataset. The model is capable of detecting various objects, including but not limited to:
+
+   * Person
+   * Bicycle
+   * Car
+   * Dog
+   * Cat
+
+The COCO_INSTANCE_CATEGORY_NAMES array contains the list of object classes the model can detect.
+
+## Images Used
+The following images are used for object detection in this project:
+
+   * Andrew Ng Image: andrew-ng1.jpg
+   * DL Guys Image: DLguys.jpeg
+   * Istockphoto Image: istockphoto-187786732-612x612.jpeg
+   * Quantas Car vs Plane Image: quantas-car-v-plane-TAKE-OFF.jpg
+
+## Results
+The results include bounding boxes drawn around detected objects in each image, with the class label and detection probability displayed. The processed images are saved with bounding boxes for further analysis.
+
+### Example Results
+   * Andrew Ng Image: Detected "person" with high confidence.
+   * DL Guys Image: Detected "person" with high confidence.
+   * Istockphoto Image: Detected "dog", "cat", and "bird".
+   * Quantas Car vs Plane Image: Detected "car" and "airplane".
+
+## License
+This project is licensed under the MIT License - see the LICENSE file for details.
